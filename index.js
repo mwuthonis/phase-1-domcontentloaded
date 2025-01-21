fa87b69893854efa379ebca57061b71db1a8e794
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-01-21 18:46:42 +0300

    DomContentLoaded codegrade assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the paragraph element by its ID
+    let paragraph = document.getElementById("text");
+    
+    // Update the content of the paragraph
+    paragraph.textContent = "This is really cool!";
+});
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
